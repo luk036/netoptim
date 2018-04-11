59115b20b3268c402f060751596886511042fe4d
Live session user <luk036@gmail.com>
2018-04-11 21:37:27 +0800

    fix for python 3.6

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # netoptim
 
 [![Build Status](https://travis-ci.org/luk036/netoptim.svg?branch=master)](https://travis-ci.org/luk036/netoptim)
-[![codecov](https://codecov.io/gh/luk036/ellpy/branch/master/graph/badge.svg)](https://codecov.io/gh/luk036/ellpy)
+[![codecov](https://codecov.io/gh/luk036/netoptim/branch/master/graph/badge.svg)](https://codecov.io/gh/luk036/netoptim)
 [![Maintainability](https://api.codeclimate.com/v1/badges/6ce78bab65047bfe53d6/maintainability)](https://codeclimate.com/github/luk036/ellpy/maintainability)
 
 Network optimization python code 
--- test_neg_cycle.py
+++ test_neg_cycle.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+from __future__ import print_function
+from pprint import pprint
+
 from networkx.utils import generate_unique_node
 import networkx as nx
 from neg_cycle import *
@@ -12,15 +15,15 @@ def test_cycle():
     N = negCycleFinder(G, newnode)
     v = N.find_neg_cycle()
     assert v == 0
-    print sorted(N.pred.items())
-    print sorted(N.dist.items())
+    print (sorted(N.pred.items()))
+    print (sorted(N.dist.items()))
 
     #dist = {newnode: 0}
     #pred = {newnode: None}
     v = N.find_neg_cycle()
     assert v == 0
-    print sorted(N.pred.items())
-    print sorted(N.dist.items())
+    print (sorted(N.pred.items()))
+    print (sorted(N.dist.items()))
 
     source = 0
     #dist = {source: 0}
@@ -29,10 +32,10 @@ def test_cycle():
     M = negCycleFinder(G, source)
     v = M.find_neg_cycle()
     assert v == None
-    print sorted(M.pred.items())
-    print sorted(M.dist.items())
+    print (sorted(M.pred.items()))
+    print (sorted(M.dist.items()))
 
     v = M.find_neg_cycle()
     assert v == None
-    print sorted(M.pred.items())
-    print sorted(M.dist.items())
+    print (sorted(M.pred.items()))
+    print (sorted(M.dist.items()))
