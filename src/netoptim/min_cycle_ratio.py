cc13f0fb956c9ae237b8fb035b531d76c2aec465
Wai-Shing Luk <luk036@gmail.com>
2023-06-21 19:53:27 +0800

    type annotation

--- src/netoptim/min_cycle_ratio.py
+++ src/netoptim/min_cycle_ratio.py
@@ -1,28 +1,32 @@
-from .parametric import MaxParametricSolver, ParametricAPI, V, R
-from typing import Tuple, Any, Generic, List
-from typing import MutableMapping, Mapping, TypeVar
 from fractions import Fraction
+from typing import Generic, List, Mapping, MutableMapping, Tuple, TypeVar
+
+from .parametric import MaxParametricSolver, ParametricAPI
 
 D = TypeVar("D", int, float, Fraction)  # Comparable Ring
+R = TypeVar("R", float, Fraction)  # Comparable field
+V = TypeVar("V")
 Cycle = List[Tuple[V, V]]
+Graph = Mapping[V, Mapping[V, Mapping[str, D]]]
+GraphMut = MutableMapping[V, MutableMapping[V, MutableMapping[str, D]]]
 
 
-def set_default(gra: MutableMapping[V, MutableMapping[V, MutableMapping[str, D]]], weight: str, value: D) -> None:
+def set_default(gra: GraphMut, weight: str, value: D) -> None:
     """_summary_
 
     Args:
-        gra (MutableMapping[V, MutableMapping[V, MutableMapping[str, D]]): _description_
+        gra (Graph): _description_
         weight (str): _description_
         value (Any): _description_
     """
-    for utx in gra:
-        for vtx in gra[utx]:
-            if gra[utx][vtx].get(weight, None) is None:
-                gra[utx][vtx][weight] = value
+    for u in gra:
+        for v in gra[u]:
+            if gra[u][v].get(weight, None) is None:
+                gra[u][v][weight] = value
 
 
 class CycleRatioAPI(ParametricAPI[V, R]):
-    def __init__(self, gra: Mapping[V, Mapping[V, Mapping[str, R]]], T: type) -> None:
+    def __init__(self, gra: GraphMut, K: type) -> None:
         """_summary_
 
         Args:
@@ -30,20 +34,20 @@ class CycleRatioAPI(ParametricAPI[V, R]):
             T (type): _description_
         """
         self.gra = gra
-        self.T = T
+        self.K = K
 
-    def distance(self, ratio: R, edge: Tuple[V, V]) -> R:
+    def distance(self, ratio: R, e: Tuple[V, V]) -> R:
         """[summary]
 
         Arguments:
             ratio ([type]): [description]
-            edge ([type]): [description]
+            e ([type]): [description]
 
         Returns:
             [type]: [description]
         """
-        utx, vtx = edge
-        return self.gra[utx][vtx]["cost"] - ratio * self.gra[utx][vtx]["time"]
+        u, v = e
+        return self.gra[u][v]["cost"] - ratio * self.gra[u][v]["time"]
 
     def zero_cancel(self, cycle: Cycle) -> R:
         """Calculate the ratio of the cycle
@@ -54,9 +58,9 @@ class CycleRatioAPI(ParametricAPI[V, R]):
         Returns:
             R: _description_
         """
-        total_cost = sum(self.gra[utx][vtx]["cost"] for (utx, vtx) in cycle)
-        total_time = sum(self.gra[utx][vtx]["time"] for (utx, vtx) in cycle)
-        return self.T(total_cost) / total_time
+        total_cost = sum(self.gra[u][v]["cost"] for (u, v) in cycle)
+        total_time = sum(self.gra[u][v]["time"] for (u, v) in cycle)
+        return self.K(total_cost) / total_time
 
 
 class MinCycleRatioSolver(Generic[V, R]):
@@ -70,7 +74,7 @@ class MinCycleRatioSolver(Generic[V, R]):
              for all (u, v) in E
     """
 
-    def __init__(self, gra: Mapping[V, Mapping[V, Any]]) -> None:
+    def __init__(self, gra: Graph) -> None:
         """_summary_
 
         Args:
@@ -88,8 +92,6 @@ class MinCycleRatioSolver(Generic[V, R]):
         Returns:
             Tuple[R, Cycle]: _description_
         """
-        # set_default(self.gra, "cost", 1)
-        # set_default(self.gra, "time", 1)
         omega = CycleRatioAPI(self.gra, type(r0))
         solver = MaxParametricSolver(self.gra, omega)
         ratio, cycle = solver.run(dist, r0)
