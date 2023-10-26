01451fadc576f54b4b8bf114072fbf84ddd3f927
Wai-Shing Luk <luk036@gmail.com>
2023-10-26 17:15:24 +0800

    update example optimal scaling matrix

--- tests/test_optscaling_raw.py
+++ tests/test_optscaling_raw.py
@@ -22,12 +22,21 @@ def test_optscaling_raw():
 
     Returns:
         [type]: [description]
+
+    A = [[ 0,  0, 22, 16, 15],
+         [ 0, 10, 20, 14,  0],
+         [25, 19, 13,  0,  0],
+         [18, 12,  0,  0, 24],
+         [11,  0,  0, 23, 17]]
     """
+
     gra = Lict(
         [
-            {1: (log(7.0), log(8.0)), 2: (log(5.0), log(4.0))},
-            {0: (log(8.0), log(7.0)), 2: (log(3.0), log(2.0))},
-            {0: (log(4.0), log(5.0)), 1: (log(2.0), log(3.0))},
+            {2: (log(22.0), log(25.0)), 3: (log(16.0), log(18.0)), 4: (log(15.0), log(11.0))},
+            {1: (log(10.0), log(10.0)), 2: (log(20.0), log(19.0)), 3: (log(14.0), log(12.0))},
+            {0: (log(25.0), log(22.0)), 1: (log(19.0), log(20.0)), 2: (log(13.0), log(13.0))},
+            {0: (log(18.0), log(16.0)), 1: (log(12.0), log(14.0)), 4: (log(24.0), log(23.0))},
+            {0: (log(11.0), log(15.0)), 3: (log(23.0), log(24.0)), 4: (log(17.0), log(17.0))},
         ]
     )
     lst = [cost for item in gra.values() for (cost, _) in item.values()]
@@ -36,7 +45,7 @@ def test_optscaling_raw():
     print(cmax, cmin)
     xinit = np.array([cmax, cmin])
     t = cmax - cmin
-    ellip = Ell(1.5 * t, xinit)
+    ellip = Ell(2 * t, xinit)
     dist = list(0.0 for _ in gra)
     omega = OptScalingOracle(gra, dist, get_cost)
     xbest, _, _ = cutting_plane_optim(omega, ellip, float("inf"))
